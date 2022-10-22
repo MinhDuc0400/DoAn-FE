657c833fc0247f396881e3e93f6c2d2e11e78601
duc.nm <minhduc0400@gmail.com>
2022-10-23 00:11:07 +0700

    update post list create post conversation chat ui

--- src/app/@theme/theme.module.ts
+++ src/app/@theme/theme.module.ts
@@ -27,7 +27,7 @@ import {
   PluralPipe,
   RoundPipe,
   TimingPipe,
-  NumberWithCommasPipe,
+  NumberWithCommasPipe, SafePipe,
 } from './pipes';
 import {
   OneColumnLayoutComponent,
@@ -68,6 +68,7 @@ const PIPES = [
   RoundPipe,
   TimingPipe,
   NumberWithCommasPipe,
+  SafePipe,
 ];
 
 @NgModule({
